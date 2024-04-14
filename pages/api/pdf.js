ea0c4041ffdea8be006a69102d21c3c2a727a0f7
= <=>
2024-04-14 19:03:13 -0300

    API with other method

--- pages/api/pdf.js
+++ pages/api/pdf.js
@@ -1,43 +1,46 @@
-import { chromium } from 'playwright';
+
+import { exec } from 'child_process';
 
 export default async function handler(req, res) {
+  if (req.method === 'POST') {
     try {
-        // Get dynamic data from the request body
-        const { name, age } = req.body;
+      // Get HTML content from request body
+      const htmlContent = req.body.htmlContent;
 
-        // Launch a headless browser instance (Playwright uses Chromium by default)
-        const browser = await chromium.launch({ executablePath: '/usr/bin' });
+      // Generate PDF using wkhtmltopdf
+      const pdfBuffer = await generatePDF(htmlContent);
 
-
-        // Create a new page in the browser
-        const page = await browser.newPage();
-
-        // Construct HTML content with dynamic data
-        const htmlContent = `
-            <html>
-            <head><title>Generated PDF</title></head>
-            <body>
-                <h1>Hello, ${name}!</h1>
-                <p>You are ${age} years old.</p>
-            </body>
-            </html>
-        `;
-
-        // Set the HTML content of the page
-        await page.setContent(htmlContent);
-
-        // Generate PDF from the HTML content
-        const pdfBuffer = await page.pdf();
-
-        // Close the browser
-        await browser.close();
-
-        // Send the generated PDF buffer as a response
-        res.setHeader('Content-Type', 'application/pdf');
-        res.setHeader('Content-Disposition', 'attachment; filename=generated_pdf.pdf');
-        res.status(200).send(pdfBuffer);
+      // Send PDF as response
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader('Content-Disposition', 'attachment; filename=generated_pdf.pdf');
+      res.status(200).send(pdfBuffer);
     } catch (error) {
-        console.error('Error:', error);
-        res.status(500).send('Internal Server Error');
+      console.error('Error generating PDF:', error);
+      res.status(500).send('Internal Server Error');
     }
+  } else {
+    res.status(405).send('Method Not Allowed');
+  }
+}
+
+// Function to generate PDF using wkhtmltopdf
+function generatePDF(htmlContent) {
+  return new Promise((resolve, reject) => {
+    const command = `wkhtmltopdf - -`;
+    const childProcess = exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error('Error generating PDF:', error);
+        reject(error);
+      } else if (stderr) {
+        console.error('stderr:', stderr);
+        reject(stderr);
+      } else {
+        resolve(stdout);
+      }
+    });
+
+    // Write HTML content to stdin of wkhtmltopdf process
+    childProcess.stdin.write(htmlContent);
+    childProcess.stdin.end();
+  });
 }
