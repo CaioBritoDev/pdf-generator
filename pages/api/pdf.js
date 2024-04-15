7263f3d9ec1a7d1a949e8eecde120d1489910188
= <=>
2024-04-14 23:49:54 -0300

    Test PDF API

--- pages/api/pdf.js
+++ pages/api/pdf.js
@@ -26,7 +26,7 @@ export default async function handler(req, res) {
 // Function to generate PDF using wkhtmltopdf
 function generatePDF(htmlContent) {
   return new Promise((resolve, reject) => {
-    const command = `/usr/local/bin/wkhtmltopdf - -`;
+    const command = `echo "${htmlContent}" | wkhtmltopdf - -`;
     const childProcess = exec(command, (error, stdout, stderr) => {
       if (error) {
         console.error('Error generating PDF:', error);
