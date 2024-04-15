c892ed5cf0db7a9f7d77fc0a4c35f84792f15496
= <=>
2024-04-15 00:23:06 -0300

    With simple

--- pages/api/pdf.js
+++ pages/api/pdf.js
@@ -1,46 +1,22 @@
 
-import { exec } from 'child_process';
+import { exec, execSync } from 'child_process';
+import wkhtmltopdf from 'wkhtmltopdf';
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
+  //if (req.method === 'POST') {
     try {
       // Get HTML content from request body
-      const htmlContent = req.body.htmlContent;
+      const htmlContent = req.body.htmlContent ? req.body.htmlContent : "<html><h1>Hello, World!</h1></html>";
 
       // Generate PDF using wkhtmltopdf
-      const pdfBuffer = await generatePDF(htmlContent);
+      wkhtmltopdf(htmlContent)
+        .pipe(res);
 
-      // Send PDF as response
-      res.setHeader('Content-Type', 'application/pdf');
-      res.setHeader('Content-Disposition', 'attachment; filename=generated_pdf.pdf');
-      res.status(200).send(pdfBuffer);
     } catch (error) {
       console.error('Error generating PDF:', error);
       res.status(500).send('Internal Server Error');
     }
-  } else {
-    res.status(405).send('Method Not Allowed');
-  }
-}
-
-// Function to generate PDF using wkhtmltopdf
-function generatePDF(htmlContent) {
-  return new Promise((resolve, reject) => {
-    const command = `/usr/local/bin/wkhtmltopdf - -`;
-    const childProcess = exec(command, (error, stdout, stderr) => {
-      if (error) {
-        console.error('Error generating PDF:', error);
-        reject(error);
-      } else if (stderr) {
-        console.error('stderr:', stderr);
-        reject(stderr);
-      } else {
-        resolve(stdout);
-      }
-    });
-
-    // Write HTML content to stdin of wkhtmltopdf process
-    childProcess.stdin.write(htmlContent);
-    childProcess.stdin.end();
-  });
+  //} else {
+   // res.status(405).send('Method Not Allowed');
+  //}
 }
